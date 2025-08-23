9a153e34013c560cfb69f02b2ff03a247ea480ef
igor bruno <igordf2502@gmail.com>
2025-08-23 03:12:58 -0300

    HelloWordJavaSpring

--- Vendas2/src/main/java/io/github/dilluter/VendasAplication.java
+++ Vendas2/src/main/java/io/github/dilluter/VendasAplication.java
@@ -2,9 +2,17 @@ package io.github.dilluter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasAplication {
+    @GetMapping
+    public String HelloWorld() {
+        return "Hello World";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
     }
