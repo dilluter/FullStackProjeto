35798687cf6797bef4cc15a3df8263d2f4b11ecb
igor bruno <igordf2502@gmail.com>
2025-08-23 02:51:17 -0300

    JAVASPRING

---
+++ Vendas2/src/main/java/io/github/dilluter/VendasAplication.java
@@ -0,0 +1,11 @@
+package io.github.dilluter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasAplication.class, args);
+    }
+}
