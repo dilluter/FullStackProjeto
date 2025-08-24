7a9e58b13a202c15eddfedac537319bdd7624d47
igor bruno <igordf2502@gmail.com>
2025-08-24 00:43:15 -0300

    configuração inicial com bean

---
+++ Vendas2/src/main/java/io/github/dilluter/AppConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.dilluter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class AppConfiguration {
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
